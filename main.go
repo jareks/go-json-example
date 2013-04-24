ff25ad66e4482040e120e89428e39467401c9e8d
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2013-04-24 18:51:16 +0200

    improvment

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import(
-  "fmt"
   "encoding/json"
   "bufio"
   "os"
@@ -11,10 +10,12 @@ func main() {
   for {
     reader := bufio.NewReader(os.Stdin)
     message, _ := reader.ReadString('\n')
+    message = message[0:len(message)-1]
     jsonMap := make(map[string]string)
     jsonMap["body"] = message
     json, _ := json.Marshal(jsonMap)
-    fmt.Printf("%s\n", json)
+    os.Stdout.Write(json)
+    os.Stdout.Write([]byte("\n"))
   }
 }
 /*
